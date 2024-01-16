8c1ac3e380931ca875c872de1d081346e4dc62e8
Brandon Luffman <brandonluff10@gmail.com>
2024-01-16 11:50:42 -0500

    removed affiliate

--- pages/affiliate.js
+++ pages/affiliate.js
@@ -30,10 +30,9 @@ const affiliate = () => {
           <meta name="twitter:creator" content="@USERNAME" />
       </Head>
     <Navbar />
-    <div className='affiliate-container'>
+    {/* <div className='affiliate-container'>
           <h1 className='affiliate-header green-font'>Team 1%</h1>
           <div className='affiliate-emoji'>&#129309;</div>
-          {/* <div className='flexer'> */}
           <div className='flexer'>
           <p className='affiliate-p'>Get ready to elevate your fitness journey with our exceptional gym equipment lineup. We are a dedicated community of individuals committed to self-improvement, pushing ourselves beyond limits to achieve greatness. Embracing sacrifice and relentless dedication, we forge ahead on the path to becoming the best versions of ourselves. Join us in our pursuit of excellence, as we break barriers and conquer new fitness heights. Together, let&apos;s unleash our full potential, fueled by the power of Danger Fit and the spirit of Team 1%.</p>
                    <img src='/hall.png' className='affiliate-img'></img>
@@ -41,15 +40,15 @@ const affiliate = () => {
           </div>
           <div className='flexer'>
           <img src='/affiliate-banner.png' className='affiliate-img affiliate-img-2'></img>
-          {/* </div> */}
           <p className='affiliate-p'>Starting out as an affiliate for Danger Fit you&apos;ll earn a competitive 10% commission for each sale generated through your unique affiliate links. By leveraging your marketing skills, building an engaged audience, and driving traffic to Danger Fit&apos;s website, you&apos;ll have the potential to earn a significant income. As you grow and demonstrate your value, there may be opportunities to increase your commission rate and unlock even greater earning potential.</p>
-          {/* <p className='affiliate-p'>Once you have signed up here, please log in to https://www.affiliatly.com/ and check out your Affiliate dashboard. If you need any help, this link will be able to guide you through Affiliatly: https://affiliatly.helpscoutdocs.com/article/137-affiliate-panel-overview</p> */}
     </div>
           <AffiliateForm />
-        </div>
+        </div> */}
     <Footer />
     </>
   )
 }
 
 export default affiliate
+
+          {/* <p className='affiliate-p'>Once you have signed up here, please log in to https://www.affiliatly.com/ and check out your Affiliate dashboard. If you need any help, this link will be able to guide you through Affiliatly: https://affiliatly.helpscoutdocs.com/article/137-affiliate-panel-overview</p> */}
